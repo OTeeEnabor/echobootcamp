447cc9e1533d7e0c70ee0f6d53ee386e820c4720
unknown <tobienabor@gmail.com>
2023-09-19 20:51:10 +0200

    changes to detail router

--- router/http/detail_router.go
+++ router/http/detail_router.go
@@ -4,7 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/OTeeEnabor/echobootcamp/controller/context/pages"
 )
-
+// details router
 func DetailsRouter(app *echo.Echo) {
 	app.GET("/:productId", pages.DetailsContext)
 }
